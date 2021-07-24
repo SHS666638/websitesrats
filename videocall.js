db10a23e47043d675d86242222219f155009262e
SHS666638 <85347510+SHS666638@users.noreply.github.com>
2021-07-24 11:32:38 +0700

    Create videocall.js

---
+++ videocall.js
@@ -0,0 +1,25 @@
+var Notify.Pick = VideoCall.id
+var Package = page.Package {
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+var PackageNotify.pack = Aux {
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+}
+var PackageNotify.pack = Aux2 {
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+}
+var PackageNotify.pack = Video {
+var StartVideo = call.systemCall()
+}
+}
