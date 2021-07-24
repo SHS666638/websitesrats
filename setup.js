12425b68b9fb38635c4b6615e461e25968681466
SHS666638 <85347510+SHS666638@users.noreply.github.com>
2021-07-24 11:18:54 +0700

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,6 @@
+var Notify.server = notify.server.HTTP
+var Notify.send = notifySend(Send!)
+var Notify.error = notifySend.Error(Error!)
+let Notify.Server = gmailstatsapi.herokuapp.com
+var notify = Notify.Switch.AudioMessage(fm.wav)
+var botmessageblock = NotifyNew(User Blocked You, You Don't Send Message!)
