7086d213251718a445c24831e17a15cfb740a9b6
SHS666638 <85347510+SHS666638@users.noreply.github.com>
2021-07-24 11:26:55 +0700

    Update and rename package.json to package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+var PackageNotify.pack = Aux {
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+}
+var PackageNotify.pack = Aux2 {
+var Notify.package = AudioPack(Send.wav)
+var Notify.package = AudioPack(Error.wav)
+var Notify.package = AudioPack(Call.wav)
+var Notify.package = AudioPack(Notify.wav)
+var Notify.package = AudioPack(CallOut.wav)
+}
+var PackageNotify.pack = Video {
+var Notify.package = Video.Player
+}
+
